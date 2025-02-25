c111bc55abb63e330c0b9efe904d90b28d2f3ec2
im-jking <jamesianking@hotmail.com>
2025-02-24 22:56:44 -0800

    Create header for application navigation

--- db-project/src/main.tsx
+++ db-project/src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import "./index.css";
+import App from "./App.tsx";
+import Products from "./pages/Products.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="products" element={<Products />} />
+        </Routes>
+      </ThemeProvider>
+    </StrictMode>
+  </BrowserRouter>
+);
