1d4f8daf5c4e14375ff13ec0c87b998bdf2aeba7
im-jking <jamesianking@hotmail.com>
2025-04-22 16:18:29 -0700

    Backend update #4

--- backend/main.py
+++ backend/main.py
@@ -14,15 +14,16 @@ origins = [
     # "https://localhost:5173",
     # "http://127.0.0.1:5173", 
     # "https://127.0.0.1:5173"
-    "http://localhost:80",
-    "https://localhost:80",
-    "http://127.0.0.1:80", 
-    "https://127.0.0.1:80"
-    "http://cs360-lab-project:80"
-    "https://cs360-lab-project:80"
-    "http://cs360-lab-project-production.up.railway.app:*"
-    "https://cs360-lab-project-production.up.railway.app:*"
-    "https://cs360-lab-project-production.up.railway.app"
+    # "http://localhost:80",
+    # "https://localhost:80",
+    # "http://127.0.0.1:80", 
+    # "https://127.0.0.1:80"
+    # "http://cs360-lab-project:80"
+    # "https://cs360-lab-project:80"
+    # "http://cs360-lab-project-production.up.railway.app:*"
+    # "https://cs360-lab-project-production.up.railway.app:*"
+    # "https://cs360-lab-project-production.up.railway.app"
+    "*"
 ]
 
 app.add_middleware(
